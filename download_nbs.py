5de13da8a1db3dba5695c8258e3a51b6583a95f3
conor_d <conor.donovan@mycit.ie>
2018-02-02 12:18:37 +0000

    NBs Downloaded

--- download_nbs.py
+++ download_nbs.py
@@ -5,39 +5,66 @@ import os
 import urllib.request as req
 import shutil
 
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-os.mkdir('tutorial_nbs')
-
 def read_file():
+    # this text file should be stored in the same directory as this script
     with open("all_tutorial_links.txt") as f:
         f_list = list(filter(lambda x:x!='',[line.strip() for line in f]))
     return f_list
 
-def hash_index(file_conts):
-    l1 = len(file_conts)
-    hash1 = [num for num in range(len(file_conts)) if file_conts[num].startswith('# ')]+[l1]
-    hash2 = [num for num in range(len(file_conts)) if file_conts[num].startswith('## ')]+[l1]
-    hash3 = [num for num in range(len(file_conts)) if file_conts[num].startswith('### ')]+[l1]
-    inds4 = [num for num in range(len(file_conts)) if not file_conts[num].startswith('#')]
-    return hash1, hash2, hash3, inds4
-
-def create_dict1(h):
-    for num2 in range(len(h)-1):
-        list_a, list_b = h[num2], h[num2+1]
-        for num in range(len(list_a)-1):
-            d1, d2 = list_a[num], list_a[num+1]
-            print([item for item in list_b if item>d1 and item<d2])
-            
-        
-    
-    dict1 = {}
-        
+def replace_hash(str1):
+    if str1.startswith('# '):
+        return str1.replace('# ','')
+    elif str1.startswith('## '):
+        return str1.replace('## ','')
+    elif str1.startswith('### '):
+        return str1.replace('### ','')
+    else:
+        return str1
     
 def download_file(file_path):
     with req.urlopen(file_path) as response, open(os.path.basename(file_path),'wb') as out_file:
         shutil.copyfileobj(response, out_file)
         
+def fix_name(str1):
+    if ':' in str1:
+        str1 = str1.replace(':','')
+    return str1
+
+def download_all(f_list, i_dict):
+    # function is a bit messy, sorry
+    # might rewrite later
+    for key in i_dict:
+        i1 = i_dict[key]
+        dear0 = fix_name(f_list[key])
+        os.mkdir(dear0)
+        os.chdir(dear0)
+        if isinstance(i1, dict):
+            for key2 in i1:
+                i2 = i1[key2]
+                dear1 = fix_name(f_list[key2])
+                os.mkdir(dear1)
+                os.chdir(dear1)
+                if isinstance(i2, dict):
+                    for key3 in i2:
+                        i3 = i2[key3]
+                        dear2 = fix_name(f_list[key3])
+                        os.mkdir(dear2)
+                        os.chdir(dear2)
+                        [download_file(f_list[item]) for item in i3]
+                        os.chdir("../")
+                elif isinstance(i2, list):
+                    [download_file(f_list[item]) for item in i2]
+                os.chdir("../")
+        elif isinstance(i1, list):
+            [download_file(f_list[item]) for item in i1]
+        os.chdir("../")
+
+# make sure you're in same directory as this script 
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 f = read_file()
-h = hash_index(f)
-d1 = create_dict1(h)
-#download_file('http://pytorch.org/tutorials/_downloads/tensor_tutorial.ipynb')
+f2 = list(map(lambda x:replace_hash(x), f))
+ind_dict = {0:{1:list(range(2, 7)), 7:list(range(8, 12)), 12:{13:list(range(14, 16)), 16:list(range(17, 20)),
+               20:list(range(21, 25))}, 25:[26], 27:[28], 29:list(range(30, 35))}, 35:list(range(36, 42)), 42:list(range(43, 47))}
+os.mkdir('tutorial_nbs')        
+os.chdir('tutorial_nbs')
+download_all(f2, ind_dict)
